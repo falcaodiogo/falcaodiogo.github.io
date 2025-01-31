659b8e609b07c4b586af8373aa271abfd9507986
falcaodiogo <falcao.diogo@ua.pt>
2025-01-31 14:58:15 +0000

    Main page frontend animation

--- portfolio/app/example/page.tsx
+++ portfolio/app/example/page.tsx
@@ -4,6 +4,7 @@
 
 import { ReactNode } from "react";
 import { motion } from "motion/react";
+import { delay } from "motion";
 
 interface CardProps {
   children: ReactNode;
@@ -23,37 +24,79 @@ const Card = ({ children, className = "" }: CardProps) => {
 export default function Page() {
   return (
     <div className="min-h-screen text-white p-4 md:p-8 grid gap-8 grid-cols-1 md:grid-cols-3 grid-rows-7">
-      <Card className="md:col-span-3 bg-gray-800">1</Card>
+      <motion.div
+        initial={{ opacity: 0, scaleY: 0, originY: 1 }}
+        animate={{ opacity: 1, scaleY: 1 }}
+        transition={{ duration: 0.2, ease: "easeOut", delay: 1.7 }}
+        className="bg-gray-800 col-span-3 text-white p-6 rounded-2xl shadow-lg"
+      >
+        1
+      </motion.div>
 
       <div className="md:col-span-2 grid grid-cols-8 gap-8 row-span-6">
-        <Card className="bg-gray-800 col-span-5">2</Card>
+        <motion.div
+          initial={{ scaleX: 0, originX: 1 }}
+          animate={{ scaleX: 1 }}
+          transition={{ duration: 0.2, ease: "easeOut", delay: 1.8 }}
+          className="bg-gray-800 col-span-5 text-white p-6 rounded-2xl shadow-lg"
+        >
+          2
+        </motion.div>
 
         <motion.div
           initial={{ x: 0, y: 0 }}
           animate={{
-            x: ["-30%", "-30%", "0%"],
-            y: ["30%", "30%", "0%"],
-            scale: [1.5, 1, 1],
+            x: ["-30%", "-30%", "-30%", "0%"],
+            y: ["30%", "30%", "30%", "0%"],
+            scale: [1.5, 1, 1, 1],
           }}
           transition={{
-            times: [0, 0.5, 1],
+            times: [0, 0.5, 0.5, 0.5, 1],
             duration: 1.5,
             delay: 0.5,
-            velocity: 10
+            ease: "easeOut",
           }}
           className="bg-gray-800 col-span-3 text-white p-6 rounded-2xl shadow-lg"
         >
           3
         </motion.div>
 
-        <Card className="bg-gray-800 col-span-4">4</Card>
+        <motion.div
+          initial={{ scaleX: 0, scaleY: 0, originX: 1, originY: 0 }}
+          animate={{ scaleX: 1, scaleY: 1 }}
+          transition={{ duration: 0.2, ease: "easeOut", delay: 1.8 }}
+          className="bg-gray-800 col-span-4 text-white p-6 rounded-2xl shadow-lg"
+        >
+          4
+        </motion.div>
 
-        <Card className="bg-gray-800 col-span-4">5</Card>
+        <motion.div
+          initial={{ scaleY: 0, originY: 0 }}
+          animate={{ scaleY: 1 }}
+          transition={{ duration: 0.2, ease: "easeOut", delay: 1.7 }}
+          className="bg-gray-800 col-span-4 text-white p-6 rounded-2xl shadow-lg"
+        >
+          5
+        </motion.div>
       </div>
 
       <div className="grid h-full grid-rows-2 gap-8 row-span-6">
-        <Card className="bg-gray-800 row-span-2">6</Card>
-        <Card className="bg-gray-800 row-span-2">7</Card>
+        <motion.div
+          initial={{ scaleX: 0, originX: 0 }}
+          animate={{ scaleX: 1 }}
+          transition={{ duration: 0.2, ease: "easeOut", delay: 1.8 }}
+          className="bg-gray-800 row-span-2 text-white p-6 rounded-2xl shadow-lg"
+        >
+          6
+        </motion.div>
+        <motion.div
+          initial={{ scaleX: 0, scaleY: 0, originX: 0, originY: 0 }}
+          animate={{ scaleX: 1, scaleY: 1 }}
+          transition={{ duration: 0.2, ease: "easeOut", delay: 1.7 }}
+          className="bg-gray-800 row-span-2 text-white p-6 rounded-2xl shadow-lg"
+        >
+          7
+        </motion.div>
       </div>
     </div>
   );
