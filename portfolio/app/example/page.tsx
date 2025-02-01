7caee29f4456c86fed659efcc0b829cb3abd8cc4
falcaodiogo <falcao.diogo@ua.pt>
2025-02-01 23:02:45 +0000

    HomePage features new text animations and full static website

--- portfolio/app/example/page.tsx
+++ portfolio/app/example/page.tsx
@@ -3,6 +3,7 @@
 import meImage from "../assets/me.jpg";
 import Image from "next/image";
 import AnimatedCard from "../components/animatedCard";
+import { motion } from "framer-motion";
 
 export default function Page() {
   return (
@@ -13,10 +14,14 @@ export default function Page() {
         transition={{ duration: 0.2, ease: "easeOut", delay: 1.7 }}
         className="bg-primary md:col-span-3 p-6"
       >
-        <h1 className="font-archivo font-extrabold text-foreground text-2xl md:text-3xl text-center flex items-center justify-center h-full">
-          Diogo's Website Portfolio
-  
-        </h1>
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 2 }}
+          className="font-archivo font-bold text-foreground text-2xl md:text-3xl text-center flex items-center justify-center h-full"
+        >
+          Made by Diogo
+        </motion.h1>
       </AnimatedCard>
 
       <div className="md:col-span-2 pt-4 md:pt-0 grid md:grid-cols-8 gap-8 row-span-6">
@@ -24,15 +29,35 @@ export default function Page() {
           initial={{ scaleX: 0, originX: 1 }}
           animate={{ scaleX: 1 }}
           transition={{ duration: 0.2, ease: "easeOut", delay: 1.8 }}
-          className="bg-primary col-span-6 md:col-span-5 p-6 pt-12 rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300"
+          className="bg-primary col-span-6 md:col-span-5 p-14 pt-12 rounded-2xl shadow-lg transition-shadow duration-300 flex flex-col justify-between"
         >
-          <h1 className="font-sans font-extrabold text-foreground text-6xl p-8">
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 2.3 }}
+            className="font-sans font-extrabold text-foreground text-6xl"
+          >
             University Projects
-          </h1>
+          </motion.h1>
 
-          <p className="text-foreground/80 font-semibold md:text-base pt-4 p-8">
-            Click here to discover my projects in university.
-          </p>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 2.1 }}
+            className="text-foreground font-semibold md:text-base pt-6 pb-4"
+          >
+            Explore a collection of academic projects made at Universidade de
+            Aveiro during my bachelor's.
+          </motion.p>
+
+          <motion.button
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 2.2 }}
+            className="bg-variant text-foreground font-semibold p-4 rounded-xl transition-colors duration-300 w-1/3 self-start"
+          >
+            Explore
+          </motion.button>
         </AnimatedCard>
 
         <AnimatedCard
@@ -62,11 +87,21 @@ export default function Page() {
           initial={{ scaleY: 0, originY: 0 }}
           animate={{ scaleY: 1 }}
           transition={{ duration: 0.2, ease: "easeOut", delay: 1.8 }}
-          className="bg-secondary col-span-6 md:col-span-4 p-6"
+          className="bg-secondary col-span-6 md:col-span-4 p-4"
         >
-          <h1 className="font-sans font-extrabold text-foreground text-xl p-8">
-            Hello, I'm Diogo Falcão and I'm a Software Engineer at University of Aveiro.
-          </h1>
+          <div className="flex items-center justify-center h-full">
+            <motion.h1
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 2 }}
+              className="font-sans font-extrabold text-foreground text-lg p-8"
+            >
+              Hello! I'm Diogo Falcão, a Software Engineer at the University of
+              Aveiro. I'm 21 years old and in the final year of my Computer
+              Science degree at Universidade de Aveiro, Portugal. Feel free to
+              explore my website.
+            </motion.h1>
+          </div>
         </AnimatedCard>
 
         <AnimatedCard
@@ -75,9 +110,77 @@ export default function Page() {
           transition={{ duration: 0.2, ease: "easeOut", delay: 1.7 }}
           className="bg-variant col-span-6 md:col-span-4 p-6"
         >
-          <h1 className="font-sans font-extrabold text-foreground text-2xl p-8">
-            Programming Languages
-          </h1>
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 2 }}
+            className="font-sans font-extrabold text-foreground text-3xl p-8"
+          >
+            Digital Skills
+          </motion.h1>
+
+          <div className="flex flex-wrap gap-4 justify-center">
+            <div className="bg-primary p-4 rounded-xl">
+              <motion.h1
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 2.1 }}
+                className="text-foreground font-semibold"
+              >
+                Kotlin
+              </motion.h1>
+            </div>
+            <div className="bg-primary p-4 rounded-xl">
+              <motion.h1
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 2.2 }}
+                className="text-foreground font-semibold"
+              >
+                Flutter
+              </motion.h1>
+            </div>
+            <div className="bg-primary p-4 rounded-xl">
+              <motion.h1
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 2.1 }}
+                className="text-foreground font-semibold"
+              >
+                Java
+              </motion.h1>
+            </div>
+            <div className="bg-primary p-4 rounded-xl">
+              <motion.h1
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 2.3 }}
+                className="text-foreground font-semibold"
+              >
+                TypeScript
+              </motion.h1>
+            </div>
+            <div className="bg-primary p-4 rounded-xl">
+              <motion.h1
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 2.2 }}
+                className="text-foreground font-semibold"
+              >
+                React Native
+              </motion.h1>
+            </div>
+            <div className="bg-primary p-4 rounded-xl">
+              <motion.h1
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 2.1 }}
+                className="text-foreground font-semibold"
+              >
+                Figma
+              </motion.h1>
+            </div>
+          </div>
         </AnimatedCard>
       </div>
 
@@ -86,25 +189,71 @@ export default function Page() {
           initial={{ scaleX: 0, originX: 0 }}
           animate={{ scaleX: 1 }}
           transition={{ duration: 0.2, ease: "easeOut", delay: 1.8 }}
-          className="bg-secondary row-span-2 p-6"
+          className="bg-secondary row-span-2 p-14 flex flex-col justify-around"
         >
-          <h1 className="font-sans font-extrabold text-foreground text-6xl p-8">
-            My Projects
-          </h1>
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 2 }}
+            className="font-sans font-extrabold text-foreground text-6xl"
+          >
+            My Personal Projects
+          </motion.h1>
 
-          <p className="text-foreground/80 font-semibold md:text-base pt-4 p-8">
-            Click here to discover my own projects.
-          </p>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 2.3 }}
+            className="text-foreground font-semibold md:text-base pt-6"
+          >
+            Here you can find a growing collection of personal projects I've
+            been working on during my free time. It's small right now, but hey,
+            even the best things start in a modest way.
+          </motion.p>
+
+          <motion.button
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 2.1 }}
+            className="bg-variant text-foreground font-semibold p-4 rounded-xl transition-colors duration-300 w-1/3 self-start"
+          >
+            See More
+          </motion.button>
         </AnimatedCard>
+
         <AnimatedCard
           initial={{ scaleX: 0, scaleY: 0, originX: 0, originY: 0 }}
           animate={{ scaleX: 1, scaleY: 1 }}
           transition={{ duration: 0.2, ease: "easeOut", delay: 1.7 }}
           className="bg-primary row-span-2 p-6"
         >
-          <li className="text-foreground/80 font-semibold md:text-base pt-4 p-8">
-            Click here to discover my social media and github.
-          </li>
+          <ul className="list-none">
+            <li className="text-foreground font-semibold md:text-base pt-4 p-8">
+              <div className="flex gap-4 items-center justify-center">
+                <a
+                  href="https://github.com/falcaodiogo"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub Profile
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/diogofalcao2/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn Profile
+                </a>
+                <a
+                  href="mailto:falcao.diogo@ua.pt"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Email Me
+                </a>
+              </div>
+            </li>
+          </ul>
         </AnimatedCard>
       </div>
     </div>
